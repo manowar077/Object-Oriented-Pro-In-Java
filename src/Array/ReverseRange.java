134ab2f4fd661af28950de571f65c86df2d12e19
Md Manowar Alam <mdmanowar077@gmail.com>
2026-08-17 22:09:22 +0530

    up

---
+++ src/Array/ReverseRange.java
@@ -0,0 +1,30 @@
+package Array;
+
+import java.util.Scanner;
+
+public class ReverseRange {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int[] arr=new int[n];
+        for(int i = 0; i<arr.length; i++){
+            arr[i]=sc.nextInt();
+        }
+        int i=sc.nextInt();
+        int j= sc.nextInt();
+        Reverse(arr,i,j);
+
+        for(int a=0;i<arr.length;i++){
+            IO.println(arr[i]+" ");
+        }
+    }
+    public static void Reverse(int[] arr,int i,int j){
+        while(i<j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+    }
+}
