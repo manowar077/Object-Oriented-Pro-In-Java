c31e29235a6eea0622ae40714e57033bccd6390e
Md Manowar Alam <mdmanowar077@gmail.com>
2026-08-18 14:24:00 +0530

    fg

--- src/SortingAlgos/BubbleSort.java
+++ src/SortingAlgos/BubbleSort.java
@@ -3,7 +3,9 @@ package SortingAlgos;
 public class BubbleSort {
     static void main() {
         int[] arr={12,34,23,34,56,78};
-        IO.println(arr[i]+" ");
+        for(int i=0;i<arr.length;i++){
+            IO.println(arr[i]+" ");
+        }
 
     }
     public static void Bubblesort(int[] arr){
