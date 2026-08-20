6cf4b6f37d07183d64e9ac7dd3b61887ce974f5b
Md Manowar Alam <mdmanowar077@gmail.com>
2026-08-20 12:01:43 +0530

    df

--- src/SortingAlgos/BinarySearch.java
+++ src/SortingAlgos/BinarySearch.java
@@ -1,29 +1,35 @@
 package SortingAlgos;
-
 public class BinarySearch {
-    static void main() {
-        int[] arr={1,3,4,5,6,7};
-        int item=11;
-        Search(arr, item);
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 5, 6, 7};
+        int item = 7;
+
+        int result = Search(arr, item);
+
+        if (result == -1) {
+            System.out.println("Item not found");
+        } else {
+            System.out.println("Item found at index: " + result);
+        }
     }
 
-    public static void Search(int[] arr,int item){
-        int low=0;
-        int high=arr.length-1;
+    public static int Search(int[] arr, int item) {
+        int low = 0;
+        int high = arr.length - 1;
 
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(arr[mid]==item){
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == item) {
                 return mid;
+            } else if (arr[mid] > item) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
-            else if(arr[mid]>=item){
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
-
         }
+
         return -1;
     }
 }
