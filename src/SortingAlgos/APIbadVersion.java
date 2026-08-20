95f7b43db5c67367d72ffe213490ef67ccc8e70a
Md Manowar Alam <mdmanowar077@gmail.com>
2026-08-20 21:16:18 +0530

    dff

---
+++ src/SortingAlgos/APIbadVersion.java
@@ -0,0 +1,29 @@
+package SortingAlgos;
+
+public class APIbadVersion {
+    static void main() {
+      int n=97;
+      firstBadVersion(n);
+    }
+        public static int firstBadVersion(int n) {
+            int low = 1;
+            int high = n;
+
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+
+                if (isBadVersion(mid)) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+
+            return low;
+        }
+
+
+    private boolean isBadVersion(int mid) {
+        return false;
+    }
+}
