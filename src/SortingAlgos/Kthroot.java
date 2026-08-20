df6635787e993bae19605fe4187a0c76e0813bd3
Md Manowar Alam <mdmanowar077@gmail.com>
2026-08-20 20:32:31 +0530

    fgfg

--- src/SortingAlgos/Kthroot.java
+++ src/SortingAlgos/Kthroot.java
@@ -1,20 +1,29 @@
 package SortingAlgos;
 
 public class Kthroot {
-    static void main() {
-        int n=87;
-        int k=3;
+    public static void main(String[] args) {
+        int n = 87;
+        int k = 3;
+
+        System.out.println(kthroot(n, k));
     }
-    public static void kthroot(int n,int k){
-        int low=0;
-        int high=n;
-        int ans=0;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(Math.pow(mid,k)<=n){
-                ans=mid;
-                low=mid+1;
+
+    public static int kthroot(int n, int k) {
+        int low = 0;
+        int high = n;
+        int ans = 0;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (Math.pow(mid, k) <= n) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
+
+        return ans;
     }
 }
