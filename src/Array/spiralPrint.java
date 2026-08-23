d85e8c86a0f2ccdc3ee71fd089b1247333ad4354
Md Manowar Alam <mdmanowar077@gmail.com>
2026-08-23 10:51:35 +0530

    df

---
+++ src/Array/spiralPrint.java
@@ -0,0 +1,40 @@
+package Array;
+
+public class spiralPrint {
+    static void main() {
+        int[][] arr={{1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16}};
+        spiralPrint(arr);
+    }
+    public static void spiralPrint(int[][] arr){
+        int minr=0;
+        int maxr=arr.length-1;
+        int minc=0;
+        int maxc=arr[0].length-1;
+        int count=0;
+        int totalelement=arr.length*arr[0].length;
+        while(count<totalelement)
+        for(int i=minc;i<=maxc;i++){
+            IO.println(arr[minr][i]);
+            count++;
+        }
+        minr++;
+        for(int i=minr;i<=maxr;i++){
+            IO.println(arr[i][maxc]);
+            count++;
+        }
+        maxc--;
+        for(int i=maxc;i>=minc;i--){
+            IO.println(arr[maxr][i]);
+            count++;
+        }
+        maxr--;
+        for(int i=maxr;i>=minr;i--){
+            IO.println(arr[i][minc]);
+            count++;
+        }
+        minc++;
+    }
+}
