3afd7e8a4ffc816e7ce7c1f14ebb5119c666881b
Md Manowar Alam <mdmanowar077@gmail.com>
2026-08-26 21:57:37 +0530

    dhf

--- src/String/StringDemo.java
+++ src/String/StringDemo.java
@@ -1,8 +1,11 @@
 package String;
 
 public class StringDemo {
+    private static Object str1;
+
     static void main() {
         String str="hello";
         String str1=new String("hello");
+        IO.println(str1);
     }
 }
