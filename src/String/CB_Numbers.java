0395bf1d60d4438a98531e3a2404c1688a577691
Md Manowar Alam <mdmanowar077@gmail.com>
2026-08-30 23:25:59 +0530

    fgfg

--- src/String/CB_Numbers.java
+++ src/String/CB_Numbers.java
@@ -21,7 +21,7 @@ public class CB_Numbers {
         if(num==0 || num==1){
             return false;
         }
-        int[] arr={2,3,4,5,7,11,13,17,19,23,29};
+        int[] arr={2,3,4,5,7,11,13,17,19,2};
         for(int i=0;i<arr.length;i++){
             if(num==arr[i]){
                 return true;
