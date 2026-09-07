25ed10fb97d396ce751cbba3bf2193fe6c0f5859
Md Manowar Alam <mdmanowar077@gmail.com>
2026-09-07 22:24:40 +0530

    iteration

--- src/Collection/List/ArrayListDemo.java
+++ src/Collection/List/ArrayListDemo.java
@@ -19,5 +19,22 @@ public class ArrayListDemo {
         list.add(2,-6);
         IO.println(list);
 
+        //GEt
+        IO.println(list.get(4));
+
+        //Set method
+        list.set(3,90);
+        IO.println(list);
+
+        //remove
+        list.remove(2);
+        IO.println(list);
+        for(int i=0;i<list.size();i++){
+            IO.println(list.get(i)+ " ");
+        }
+        for(int v:list){
+            IO.println(v+" ");
+        }
+
     }
 }
