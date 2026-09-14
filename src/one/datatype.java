51e52af1a2665afba5a9fc990022158a536c847e
Md Manowar Alam <mdmanowar077@gmail.com>
2026-09-14 12:32:43 +0530

    jf

---
+++ src/one/datatype.java
@@ -0,0 +1,14 @@
+package one;
+
+public class datatype {
+    static void main() {
+        char ch='a';
+        int X=ch;
+        IO.println(X);
+        int a = null;       //
+        double b = null;    //
+        char c = null;      //
+        boolean d = null;   //
+    }
+}
+
