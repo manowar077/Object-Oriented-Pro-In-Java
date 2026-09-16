69db85c70183abae8d9cf1eabdc04ac8fd8468ab
Md Manowar Alam <mdmanowar077@gmail.com>
2026-09-16 09:23:40 +0530

    jf

--- src/one/datatype.java
+++ src/one/datatype.java
@@ -3,11 +3,11 @@ package one;
 public class datatype {
     static void main() {
 
-        byte a=10;
-        byte b=20;
+        int result=(25/4+3*2);
         //byte result=a+b;
         //var result1 = result;
         //IO.println(result1);
+        IO.println(result);
     }
 }
 
